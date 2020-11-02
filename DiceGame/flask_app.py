85b3c8641c0eeeb9648ae20a6cf841abf795ca41
DaliborTrampota <dalibor.trampota@gmail.com>
2020-11-02 15:26:56 +0100

    Dice game

---
+++ DiceGame/flask_app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def diceThrow():
+    plr = random.randint(1, 6)
+    pc = random.randint(1, 6)
+    if (plr > pc):
+        result="Výhra!"
+    elif (plr < pc):
+        result="Prohra!"
+    else:
+        result="Remíza!"
+
+    return render_template('hra.html', plr=plr, pc=pc, result=result)
+
+if __name__ == '__main__':
+    app.run()
